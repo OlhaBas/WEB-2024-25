33f02be7851b1a473eff03ec91fb958f3f8878ca
OlhaBas <144379158+OlhaBas@users.noreply.github.com>
2024-10-19 21:12:04 +0300

    Create register.php

---
+++ lab4/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign In</title>
+</head>
+<body>
+    <h2>Form of Register</h2>
+    <form action="register.php" method="post">
+        <label for="username">User name:</label>
+        <input type="text" name="username" required><br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br><br>
+        
+        <input type="submit" value="Sign in">
+    </form>
+</body>
+</html>
